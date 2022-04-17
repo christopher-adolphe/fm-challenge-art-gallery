ed6031966ca92956ee38ef128fa30b9301465eb5
cadolphe <christopher.adolphe@gmail.com>
2022-04-17 08:03:17 +0400

    Deploying dist directory to add new site to Netlify

---
+++ dist/main.958320a2f6737cfa7919.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";(()=>{const t=t=>document.getElementById(t),e=t=>{const e=`© ${(new Date).getFullYear()}. Created by Christopher Adolphe`;t&&(t.textContent=e)};document.addEventListener("DOMContentLoaded",(()=>{const n=t("copyright");e(n)}))})()})();
