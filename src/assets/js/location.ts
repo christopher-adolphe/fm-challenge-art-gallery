6bed84f54d92cb3ee5069fb3b80ad0f8ddb3f2fc
cadolphe <christopher.adolphe@gmail.com>
2022-04-30 09:20:03 +0400

    Added environment variable to store mapbox token

--- src/assets/js/location.ts
+++ src/assets/js/location.ts
@@ -21,20 +21,12 @@ export default function renderLocation(elem: string, lat: number, long: number, 
   
   L.marker([lat, long], { icon: markerIcon }).addTo(map);
 
-<<<<<<< HEAD
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2hyaXN0b3BoZXItYWRvbHBoZSIsImEiOiJjbDJlNzEzdWUwN21yM2lyd3NqNjVnNHh1In0.O8bormbidBxRHDDbJGRCcg', {
-=======
   L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${ACCESS_TOKEN}`, {
->>>>>>> c58ced87a7ae820307d00af3ec6d2a973de56614
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 20,
     id: 'mapbox/light-v10',
     tileSize: 512,
     zoomOffset: -1,
-<<<<<<< HEAD
-    accessToken: 'pk.eyJ1IjoiY2hyaXN0b3BoZXItYWRvbHBoZSIsImEiOiJjbDJlNzEzdWUwN21yM2lyd3NqNjVnNHh1In0.O8bormbidBxRHDDbJGRCcg'
-=======
     accessToken: ACCESS_TOKEN
->>>>>>> c58ced87a7ae820307d00af3ec6d2a973de56614
   }).addTo(map);
 }
